849ee1c4854ff9f3f40357c2be107f9296b364e6
David <davidgonzalez5910@gmail.com>
2026-03-05 14:22:22 -0500

    Primera definición de arreglos y funcionalidad que permite al usuario ingresar los datos.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        // Se definen los 3 arreglos con 7 espacios cada uno
+        int[] primerArreglo = new int[7];
+        int[] segundoArreglo = new int[7];
+        int[] tercerArreglo = new int[7];
+
+        // Se define scanner para tomar datos por consola
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Por favor ingrese uno por uno los 7 valores del primer arreglo, " +
+                "solo números serán válidos: ");
+        for(int i = 0; i < 7; i++) {
+            System.out.println("Dígito " + (i + 1) + ": ");
+            primerArreglo[i] = scanner.nextInt();
+        }
+
+
+
+
 
     }
 }
